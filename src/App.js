994406f9a3f21dd2716e54d4c822bf0494dce057
madanpandey97 <madanpandey97@gmail.com>
2020-03-30 20:46:26 +0530

    Week 1 completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap'
-import './App.css';
+import React,  {Component}  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar dark color='primary'>
-       <div className="container">
-         <NavbarBrand href='/' >
-           Confusion
-         </NavbarBrand>
-       </div>
-     </Navbar>
-    </div>
-  );
-}
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
+
+import {DISHES} from './shared/dishes';
+
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
+  render(){
+    return(
+        <div className="App">
+            <Navbar dark color='primary'>
+              <div className="container">
+                <NavbarBrand href='/' >
+                  Confusion
+                </NavbarBrand>
+              </div>
+            </Navbar>
+            <Menu  dishes = {this.state.dishes} />
+            </div>
+          );
+        }
+     
+     
+   }
+
+
+    
 
 export default App;
